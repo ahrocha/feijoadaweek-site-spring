42f7a8c2762b787ca43341bdfba4d169170df40b
Andrey <andrey.rocha@pravaler.com.br>
2021-04-27 19:53:01 -0300

    Google API Client

--- src/main/java/br/com/feijoadaweek/mvc/feijuca/repository/PratoRepository.java
+++ src/main/java/br/com/feijoadaweek/mvc/feijuca/repository/PratoRepository.java
@@ -16,21 +16,4 @@ public interface PratoRepository extends JpaRepository<Prato, Integer>{
 
 	public List<Prato> findByRestaurante(Restaurante restaurante);
 
-//	List<Prato> findByRestaurante(String slug);
-	
-//	public List<Prato> findAll();
-	
-//	public Prato findById(int id) {
-//		
-//		Prato prato = entityManager.find(Prato.class, id);
-//		
-//		return prato;
-//	}
-//	
-//	public List<Prato> findByRestaurante(Restaurante restaurante) {		
-//		Query query = entityManager.createQuery("SELECT p FROM Prato p WHERE p.restaurante = :restaurante ORDER BY data DESC ", Prato.class);
-//		query.setParameter("restaurante", restaurante);
-//		
-//		return query.getResultList();
-//	}
 }
